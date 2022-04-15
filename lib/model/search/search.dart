6c5fb654300467ca16e7b7981829eb2764356544
Mohamed-Ash <mohamed.ashraf4117@gmail.com>
2022-04-15 06:59:12 +0200

    Update search.dart

--- lib/model/search/search.dart
+++ lib/model/search/search.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors
 
 import 'package:buildcondition/buildcondition.dart';
 import 'package:flutter/material.dart';
@@ -6,49 +6,50 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app_eg/business/news_cubit.dart';
 import 'package:news_app_eg/const/network/get_news_api.dart';
 
-// class SearchScreen extends StatelessWidget {
-//   SearchScreen({Key? key}) : super(key: key);
-//   var searchController = TextEditingController();
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocConsumer<NewsCubit, NewsState>(
-//       listener: (context, state) {
-//         // TODO: implement listener
-//       },
-//       builder: (context, state) {
-//         var list =NewsCubit.get(context).search;
-//         return Scaffold(
-//           body: Column(
-//             children: [
-//               Padding(padding: const EdgeInsets.all(20.0),
-//                 child:  defaultFormField(
-//                   controller: searchController,
-//                   labelText: 'search',
-//                   validate: (String value) {
-//                     if (value.isEmpty) {
-//                       return 'must not be empty';
-//                     } else {
-//                       return null;
-//                     }
-//                   },
-//                   prefixIcon: Icons.search,
-//                   type: TextInputType.text,
-//                   onChange: (value) {
-//                     NewsCubit.get(context).getSearch(value);
-//                   },
-//                 ),
-//
-//               ),
-//               Expanded(child: articleBuilder(list,context))
-//
-//             ],
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
+/* class SearchScreen extends StatelessWidget {
+  SearchScreen({Key? key}) : super(key: key);
+  var searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<NewsCubit, NewsState>(
+      listener: (context, state) {
+       // ignore: todo
+       // TODO: implement listener
+      },
+      builder: (context, state) {
+        var list =NewsCubit.get(context).search;
+        return Scaffold(
+          body: Column(
+            children: [
+              Padding(padding: const EdgeInsets.all(20.0),
+                child:  defaultFormField(
+                  controller: searchController,
+                  labelText: 'search',
+                  validate: (String value) {
+                    if (value.isEmpty) {
+                      return 'must not be empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                  prefixIcon: Icons.search,
+                  type: TextInputType.text,
+                  onChange: (value) {
+                    NewsCubit.get(context).getSearch(value);
+                  },
+                ),
+
+              ),
+              Expanded(child: articleBuilder(list,context))
+
+            ],
+          ),
+        );
+      },
+    );
+  }
+} */
 class SearchScreen extends StatelessWidget {
   var searchController = TextEditingController();
   @override
